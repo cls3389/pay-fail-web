4dc1e6a1a23cad4a6004ec977ba1d930ce37bbed
Developer <developer@example.com>
2025-09-06 19:02:32 +0800

    Remove accidentally added dsm directory

--- dsm/stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# 优雅停止
-PIDFILE=/volume2/Cursor/excel/koukuanshibai-web/run.pid
-if [ -f "$PIDFILE" ]; then
-    kill -TERM "$(cat "$PIDFILE")" && rm -f "$PIDFILE"
-    echo "已发送 TERM，等待 worker 退出…"
-else
-    echo "未找到 PID 文件，可能未运行"
-fi
